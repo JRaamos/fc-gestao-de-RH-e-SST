8754766ef95c572232ab87d32b767148f36443fe
Jonathan <jhonyramos46@gmail.com>
2024-02-26 22:46:31 -0300

    chore: configura Tailwind e Slint no projeto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../components/Header';
 
  function Home() {
   return (
-    <div>
-      Home
+    <div className=''>
+      <header>
+        <Header/>
+      </header>
     </div>
   )
 }
